1ba3a4eac0af061924e77ea1a31d37b70f6ff61a
Miguel Angel Gtz Gomez <233382@ids.upchiapas.edu.mx>
2025-09-07 12:28:53 -0600

    First commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,13 @@
+import { inter } from "app/ui/font";
+import './ui/global.css';
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="es">
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
